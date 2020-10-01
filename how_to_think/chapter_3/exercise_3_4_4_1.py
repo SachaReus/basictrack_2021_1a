27127224c86311aaff546e7c110d84bdd185bd12
Sacha Reus <sachareus@hotmail.com>
2020-10-01 16:33:13 +0200

    exercise 3.4.4.1

---
+++ how_to_think/chapter_3/exercise_3_4_4_1.py
@@ -0,0 +1,6 @@
+today = int(input("please give me the number of the weekday (0-6)"))
+
+weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+print("It is", weekdays[today])
+
