0cf413eb8da4d3935702e7466cc12df54e881ab3
Sacha Reus <sachareus@hotmail.com>
2020-10-03 19:30:09 +0200

    exercises 3.4.4.6 set 2 (unfinished)

---
+++ how_to_think/chapter_3/set_1/exercise_3_4_4_6.py
@@ -0,0 +1,26 @@
+numbers = [83, 75, 74.9, 70, 69.9, 65, 60, 59.9, 55, 50, 49.9, 45, 44.9, 40, 39.9, 2, 0]
+
+for grade in numbers:
+    if numbers >=75:
+    print("Grade is First")
+    elif numbers [70-75):
+    print("Grade is Upper Second")
+    elif numbers [60-70):
+    print("Grade is Second")
+    elif numbers [50-60):
+    print("Grade is Third")
+    elif numbers [45-50):
+    print("Grade is F1 Supp")
+    elif numbers [40-45):
+    print("Grade is F2")
+    elif numbers <40:
+    print("Grade is F3)")
+    
+
+
+
+
+
+
+
+
