40bd56bde42e5494dbcc4342def2491a1fe71370
Sacha Reus <sachareus@hotmail.com>
2020-10-04 14:21:36 +0200

    exercise 3.4.4.4 set 3

---
+++ how_to_think/chapter_3/set_3/exercise_3_4_4_4.py
@@ -0,0 +1,12 @@
+words = ["sugar", "carrot", "corn", "eggplant", "pea", "tomato", "zucchini"]
+
+words_with_length_five = 0
+for word in words:
+    if len(word) == 5:
+        words_with_length_five += 1
+
+print("The number of words with length 5 is", words_with_length_five)
+
+
+
+
