a012e922ea6299aa95ffb11a57545d7a75f021c0
Sacha Reus <sachareus@hotmail.com>
2020-10-08 15:52:10 +0200

    exercise 3.4.4.9 set 1

---
+++ how_to_think/chapter_3/set_1/exercise_3_4_4_9.py
@@ -0,0 +1,18 @@
+# for right-angled triangle: A**2 + B**2 = C**2 must hold up
+
+length_A = int(input("please give me the length of side A"))
+length_B = int(input("please give me the length of side B"))
+length_C = int(input("please give me the length of side C"))
+
+if length_A > length_C:
+    length_A, length_C = length_C, length_A
+if length_B > length_C:
+    length_B, length_C = length_C, length_B
+
+if (int(length_A) ** 2 + int(length_B) ** 2) == int(length_C) ** 2:
+    print("True, the triangle is right-angled")
+else:
+    print("False, the triangle is not right-angled")
+
+
+
